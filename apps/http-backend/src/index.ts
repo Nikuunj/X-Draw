63f611e32c33cb00c5a96ed2f961e4a9191adb9f
Nikunj <mnikunj262@gmail.com>
2025-07-12 15:32:53 +0530

    update : router

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { authRouter } from "./routes/auth";
-import { createRoomRouter } from "./routes/createRoom";
+import { roomRouter } from "./routes/roomRouter";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/v1/auth', authRouter);
+app.use('/v1/auth', authRouter)
 
-app.use('/v1/room', createRoomRouter)
+app.use('/v1/room', roomRouter)
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
---
+++ apps/http-backend/src/routes/roomRouter.ts
@@ -0,0 +1,78 @@
+import { CreateRoomSchema } from "@repo/common/types";
+import { Request, Response, Router } from "express";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { prismaClient } from "@repo/db/client";
+
+export const roomRouter: Router = Router();
+
+
+roomRouter.post('/create-room', authMiddleware, async (req: Request, res: Response) => {
+     const verify = CreateRoomSchema.safeParse(req.body);
+     if(!verify.success) {
+          res.json({
+               msg: 'plz pass valid input'
+          }).status(422)
+          return
+     }
+
+     const userId = req.userId ?? "";
+
+     try {
+          const room = await prismaClient.room.create({
+               data: {
+                    slug: verify.data.name,
+                    adminId: userId
+               }
+          })
+          res.status(200).json({
+               roomId: room.id
+          });
+     } catch (e) {
+          res.status(500).json({
+               massege: 'Internal server Error / User not found'
+          });
+     }
+})
+
+roomRouter.get('/chat/:roomId', authMiddleware, async (req: Request, res: Response) => {
+     const roomId = Number(req.params.roomId);
+     const masseges = await prismaClient.chatHistory.findMany({
+          where: {
+               roomId
+          },
+          orderBy : {
+               id: "desc"
+          }, 
+          take: 50
+     })
+     res.json({
+          masseges
+     })
+})
+
+roomRouter.get('/room/:slug', authMiddleware, async (req: Request, res: Response) => {
+     const slug = req.params.slug;
+     try {
+
+
+          const room = await prismaClient.room.findFirst({
+               where: {
+                    slug
+               },
+          })
+
+          if(!room) {
+               res.status(404).json({   
+                    massege: 'Room not found'
+               })
+               return;
+          }
+          res.json({
+               roomId: room.id
+          })
+     } catch (e) {
+          res.status(404).json({
+               massege: 'Room not found'
+          })
+     }
+})
