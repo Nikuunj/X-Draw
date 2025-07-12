3ae93f93dbc52b50f054bd4e16f25eb2bc3fe218
Nikunj <mnikunj262@gmail.com>
2025-07-13 00:26:21 +0530

    fix: cors issue

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authRouter } from "./routes/auth";
 import { roomRouter } from "./routes/roomRouter";
-
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/v1/auth', authRouter)
 
