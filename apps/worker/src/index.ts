ee142a22f512917259f157a77b62afec83fcb296
Nikunj <mnikunj262@gmail.com>
2025-12-04 18:36:04 +0530

    update: worker for update db

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,9 +1,22 @@
 
 import { createClient } from "redis";
 const client = createClient();
+import { prismaClient } from '@repo/db/client'
 
 async function processSubmission(submission: string) {
-  console.log(submission);
+  // console.log(submission);
+  const parseObj = JSON.parse(submission)
+  try {
+    await prismaClient.chatHistory.create({
+      data: {
+        userId: parseObj.userId,
+        roomId: parseObj.roomId,
+        message: parseObj.message
+      }
+    })
+
+  } catch (e) {
+  }
 }
 
 async function startWorker() {
@@ -15,8 +28,11 @@ async function startWorker() {
     // Main loop
     while (true) {
       try {
-        const submission = await client.brPop("problems", 0);
-        // @ts-ignore
+        const submission = await client.brPop("chatHistory", 0);
+
+        if (!submission?.element) {
+          return;
+        }
         await processSubmission(submission.element);
       } catch (error) {
         console.error("Error processing submission:", error);
