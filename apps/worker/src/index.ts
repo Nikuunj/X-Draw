e7b958a98a7f7b3682ea0a8b8cb639f94b2f2907
Nikunj <mnikunj262@gmail.com>
2025-12-04 17:19:39 +0530

    update: redis worker

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,2 +1,32 @@
-console.log('hi');
 
+import { createClient } from "redis";
+const client = createClient();
+
+async function processSubmission(submission: string) {
+  console.log(submission);
+}
+
+async function startWorker() {
+
+  try {
+    await client.connect();
+    console.log("Worker connected to Redis.");
+
+    // Main loop
+    while (true) {
+      try {
+        const submission = await client.brPop("problems", 0);
+        // @ts-ignore
+        await processSubmission(submission.element);
+      } catch (error) {
+        console.error("Error processing submission:", error);
+        // Implement your error handling logic here. For example, you might want to push
+        // the submission back onto the queue or log the error to a file.
+      }
+    }
+  } catch (error) {
+    console.error("Failed to connect to Redis", error);
+  }
+}
+
+startWorker();
