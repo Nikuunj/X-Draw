34a57d7d6094d19d19e673f744da550b7d95c3fd
Nikunj <mnikunj262@gmail.com>
2025-12-04 14:01:48 +0530

    fix: compiler error js error

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,22 +1,21 @@
 import { z } from "zod";
 
 export const CrateUserSchema = z.object({
-     username: z.string().min(3).max(20).email(),
-     password: z.string().min(6).max(100),
-     name: z.string().min(1).max(50)
+  username: z.string().min(3).max(20).email(),
+  password: z.string().min(6).max(100),
+  name: z.string().min(1).max(50)
 })
 
 export const SignInUserSchema = CrateUserSchema.pick({
-     username: true,
-     password: true,
+  username: true,
+  password: true,
 })
 
 export const CreateRoomSchema = CrateUserSchema.pick({
-     name: true
+  name: true
 })
 
 export type signinType = z.infer<typeof SignInUserSchema>
- 
-export type signupType = z.infer<typeof CrateUserSchema>
 
+export type signupType = z.infer<typeof CrateUserSchema>
 export type createRoomType = z.infer<typeof CreateRoomSchema>
