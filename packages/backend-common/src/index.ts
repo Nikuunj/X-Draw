541774b3347e485d762c050d17b9002c6a1296c6
Nikunj <mnikunj262@gmail.com>
2025-12-05 15:52:46 +0530

    load: dotenv

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,1 +1,6 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: "../../.env" });
+
 export const JWT_SECRET = process.env.JWT_SECRET || 'default_secret';
+export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379"
