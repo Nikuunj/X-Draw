d98d3c958c862503ae04332abafd8a3197cb187a
Nikunj <mnikunj262@gmail.com>
2025-12-06 18:17:25 +0530

    update: http-backend docker file

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -12,5 +12,5 @@ app.use('/v1/auth', authRouter)
 app.use('/v1/room', roomRouter)
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+  console.log("Server is running on port 3001");
 });
