3142e2d53196f9fa846f7d1da73592cbdb7a44aa
Nikunj <mnikunj262@gmail.com>
2025-12-13 01:52:50 +0530

    fix: it for seperate container deployment

--- apps/x-draw/config/config.ts
+++ apps/x-draw/config/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_URL = process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:30011111111111111111111";
-export const WS_URL = process.env.NEXT_PUBLIC_HTTP_URL || "ws://localhost:80800000000000000000000";
+export const HTTP_URL = process.env.NEXT_PUBLIC_HTTP_URL || "http://localhost:3001";
+export const WS_URL = process.env.NEXT_PUBLIC_HTTP_URL || "ws://localhost:8080";
